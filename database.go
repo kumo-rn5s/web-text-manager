46e5a3fdbe50c3c01798a69c67e3143add9ed7ca
FirosStuart <k934277599@gmail.com>
2021-04-04 02:25:06 +0900

    gorm added

--- database.go
+++ database.go
@@ -1,9 +1,92 @@
 package main
 
 import (
+	"encoding/json"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/sessions"
+	"github.com/kataras/iris/v12/sessions/sessiondb/redis"
 	"io/ioutil"
+	"os"
+	"time"
 )
+
+var MYSQLDB *gorm.DB
+var REDISDB *redis.Database
+
+type dbConfiguration struct {
+	DBHost    string	`json:"dbHost"`
+	DBPort    string	`json:"dbPort"`
+	DBName    string	`json:"dbName"`
+	DBUser    string	`json:"dbUser"`
+	DBPass    string	`json:"dbPass"`
+}
+
+type User struct {
+	Username    string   `json:"user" gorm:"primary_key"`
+	Password    string   `json:"pass"`
+}
+
+//type CreateBookInput struct {
+//	Title  string `json:"title" binding:"required"`
+//	Author string `json:"author" binding:"required"`
+//}
+//
+//type UpdateBookInput struct {
+//	Title  string `json:"title"`
+//	Author string `json:"author"`
+//}
+
+
+func ConnectRedis() *sessions.Sessions {
+
+	REDISDB = redis.New(redis.Config{
+		Network:   "tcp",
+		Addr:      getEnv("REDIS_ADDR", "localhost:6379"),
+		//Addr:      getEnv("REDIS_ADDR", "redis:6379"),
+		Timeout:   time.Duration(30) * time.Second,
+		MaxActive: 10,
+		Username:  "",
+		Password:  "",
+		Database:  "",
+		Prefix:    "mds-",
+		Driver:    redis.GoRedis(), // defaults.
+	})
+	defer REDISDB.Close() // close the database connection if application errored.
+
+	sess := sessions.New(sessions.Config{
+		Cookie:          "md_session",
+		Expires:         -1, // defaults to -1: delete by closing browser
+		AllowReclaim:    true,
+		CookieSecureTLS: true,
+	})
+	sess.UseDatabase(REDISDB)
+	return sess
+}
+
+func ConnectMySQL() {
+
+	file, _ := os.Open("conf.json")
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	var dbConf dbConfiguration
+	_ = decoder.Decode(&dbConf)
+	dst := dbConf.DBUser+":"+dbConf.DBPass+"@tcp("+dbConf.DBHost+":"+dbConf.DBPort+")/"+dbConf.DBName
+
+	database, err := gorm.Open("mysql", dst+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(&User{})
+	// CREATE Table
+
+	MYSQLDB = database
+}
+
 
 func createJSON()iris.Map{
 	// Function Arguments need to contain db access infos
@@ -28,3 +111,82 @@ func createJSON()iris.Map{
 		"task_list" : DBTaskList,
 	}
 }
+
+
+
+//
+//// GET /books
+//// Find all books
+//func FindBooks(ctx iris.Context) {
+//	var books []Book
+//	MYSQLDB.Find(&books)
+//
+//	ctx.JSON(http.StatusOK, gin.H{"data": books})
+//}
+//
+//// GET /books/:id
+//// Find a book
+//func FindBook(ctx iris.Context) {
+//	// Get model if exist
+//	var book Book
+//	if err := MYSQLDB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
+//		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+//		return
+//	}
+//
+//	ctx.JSON(http.StatusOK, gin.H{"data": book})
+//}
+//
+//// POST /books
+//// Create new book
+//func CreateBook(ctx iris.Context) {
+//	// Validate input
+//	var input CreateBookInput
+//	if err := ctx.ShouldBindJSON(&input); err != nil {
+//		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//		return
+//	}
+//
+//	// Create book
+//	book :=  Book{Title: input.Title, Author: input.Author}
+//	 MYSQLDB.Create(&book)
+//
+//	ctx.JSON(http.StatusOK, gin.H{"data": book})
+//}
+//
+//// PATCH /books/:id
+//// Update a book
+//func UpdateBook(ctx iris.Context) {
+//	// Get model if exist
+//	var book  Book
+//	if err :=  MYSQLDB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
+//		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+//		return
+//	}
+//
+//	// Validate input
+//	var input UpdateBookInput
+//	if err := ctx.ShouldBindJSON(&input); err != nil {
+//		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//		return
+//	}
+//
+//	 MYSQLDB.Model(&book).Updates(input)
+//
+//	ctx.JSON(http.StatusOK, gin.H{"data": book})
+//}
+//
+//// DELETE /books/:id
+//// Delete a book
+//func DeleteBook(ctx iris.Context) {
+//	// Get model if exist
+//	var book  Book
+//	if err :=  MYSQLDB.Where("id = ?", ctx.Param("id")).First(&book).Error; err != nil {
+//		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+//		return
+//	}
+//
+//	 MYSQLDB.Delete(&book)
+//
+//	ctx.JSON(http.StatusOK, gin.H{"data": true})
+//}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/FirosStuart/web-text-manager
 
 go 1.16
 
-require github.com/kataras/iris/v12 v12.2.0-alpha2.0.20210325103852-ef22f7c4fad2 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/kataras/iris/v12 v12.2.0-alpha2.0.20210325103852-ef22f7c4fad2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	gorm.io/gorm v1.21.6 // indirect
+)
